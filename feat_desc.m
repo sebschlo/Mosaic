61b3de86c147b71ed4ff8eaea3ec9a3dbde64cb8
Sebastian <s.schloesser@gmail.com>
2014-11-18 00:10:12 -0500

    implemented feat_desc.m

--- feat_desc.m
+++ feat_desc.m
@@ -1,7 +1,38 @@
 function [ p ] = feat_desc( im, y, x )
 %FEAT_DESC Summary of this function goes here
-%   Detailed explanation goes here
+%   This function will extract the feature descriptors of the imag
+%   based on the corners found by the Harris algorithm and filtered
+%   by adaptive non-maximum suppression.
 
+% create p: a 64 x n array that contains the descriptors of each
+% corner made into a column vector
+n = length(y);
+p = zeros(64,n);
 
+% Iterate through every point
+for i=1:n
+    % Sample 40x40 pixel square around point and blur
+    try
+        square = im(y(i)-19:y(i)+20, x(i)-19:x(i)+20);
+    catch
+        % If the square results in out of bounds, continue to
+        % the next loop iteration to skip this corner. A col
+        % of zeros will remain in this corner's place. 
+        continue;
+    end
+    filter = fspecial('gaussian');
+    square = imfilter(square, filter, 'symmetric');
+    
+    % Subsample to reduce to 8x8 pixels
+    subSam = square(1:5:end,1:5:end);
+    
+    % Bias/Gain Normalize
+    subSam = double(subSam);
+    subSam = subSam - mean2(subSam);
+    subSam = subSam / std2(subSam);
+    
+    % Make into column vector and place in p
+    p(:,i) = subSam(:);
 end
 
+end
--- sebs_demo.m
+++ sebs_demo.m
@@ -5,15 +5,15 @@
 
 
 %% Load images to be stitched
-im1 = imread('21.jpg');
-im2 = imread('22.jpg');
+im1  = rgb2gray(im2double(imread('21.jpg')));
+im2  = rgb2gray(im2double(imread('22.jpg')));
 
 %% Corner Detection
 % subplot(1,3,1);
-imshow(uint8(im1));
+imshow(im1);
 title('Original Image');
 
-C = cornermetric(rgb2gray(im1));
+C = cornermetric(im1);
 % subplot(1,3,2);
 % imshow(C);
 title('Harris Corner Detection');
@@ -24,3 +24,8 @@ title('Harris Corner Detection');
 hold on
 scatter(x, y, 'ro');
 
+%% Extrac Feature Descriptors
+
+p = feat_desc(im1g, y, x);
+
+%% Descriptor Matching
